c1483ef7e973ced895eb0da3b27bb86eb0483da3
Miguel Zilhao <miguel.zilhao.nogueira@tecnico.ulisboa.pt>
2019-10-07 15:36:48 +0100

    output: make it nicer

--- src/output.jl
+++ src/output.jl
@@ -6,12 +6,13 @@ function out_info(it::Integer, t::Real, f, label::String, info_every::Integer,
 
     if it % header_every == 0
         println("--------------------------------------------------------------------------------")
-        println(" it \t   Time     \t  min($label) \t max($label)")
+        println("Iteration      Time |              \t $label")
+        println("                    |   minimum      maximum")
         println("--------------------------------------------------------------------------------")
     end
 
     if it % info_every == 0
-        @printf " %d \t %6.3f \t %g \t %g\n" it t minimum(f) maximum(f)
+        @printf "%9d %9.3f | %9.4f    %9.4f\n" it t minimum(f) maximum(f)
     end
 
     nothing
