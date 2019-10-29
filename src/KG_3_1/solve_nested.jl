166e263c6d3e3a15e0e28444c2da339129acbd2a
Miguel Zilhao <miguel.zilhao.nogueira@tecnico.ulisboa.pt>
2019-10-29 16:54:25 +0000

    solve_nested: continue to adapt to multi-domains

--- src/KG_3_1/solve_nested.jl
+++ src/KG_3_1/solve_nested.jl
@@ -8,11 +8,20 @@ function solve_lin_system!(sol, A_mat, b_vec)
     nothing
 end
 
-function nested_g1(sys)
+struct Nested{S,D,T<:Real}
+    sys     :: S
+    uu      :: Vector{T}
+    xx      :: Vector{T}
+    yy      :: Vector{T}
+    Du_phi  :: D
+    Dxx_phi :: D
+    Dyy_phi :: D
+    A_mat   :: Matrix{T}
+    b_vec   :: Vector{T}
+    vars    :: AllVars{T}
+end
+function Nested(sys::System)
     coords = sys.coords
-    uderiv = sys.uderiv
-    xderiv = sys.xderiv
-    yderiv = sys.yderiv
 
     uu, xx, yy = Vivi.xx(coords)
     Nu = length(uu)
@@ -25,70 +34,92 @@ function nested_g1(sys)
 
     A_mat = zeros(Nu, Nu)
     b_vec = zeros(Nu)
+    vars  = AllVars{eltype(A_mat)}()
+
+    Nested{typeof(sys), typeof(Du_phi),
+           eltype(A_mat)}(sys, uu, xx, yy, Du_phi, Dxx_phi, Dyy_phi, A_mat, b_vec, vars)
+end
+
+Nested(systems::Vector) = [Nested(sys) for sys in systems]
+
+
+function nested_g1!(nested::Nested, bulk::BulkVars, boundary::BulkVars)
+    sys  = nested.sys
+    uu   = nested.uu
+    xx   = nested.xx
+    yy   = nested.yy
+
+    Du_phi  = nested.Du_phi
+    Dxx_phi = nested.Dxx_phi
+    Dyy_phi = nested.Dyy_phi
+
+    A_mat   = nested.A_mat
+    b_vec   = nested.b_vec
+    vars    = nested.vars
+
+    uderiv = sys.uderiv
+    xderiv = sys.xderiv
+    yderiv = sys.yderiv
+
+    Vivi.D!(Du_phi, bulk.phi, uderiv, 1)
+    Vivi.D2!(Dxx_phi, bulk.phi, xderiv, 2)
+    Vivi.D2!(Dyy_phi, bulk.phi, yderiv, 3)
+
     ABCS  = zeros(4)
-    vars  = AllVars{Float64}()
 
-    function (bulk::BulkVars, boundary::BulkVars)
-
-        Vivi.D!(Du_phi, bulk.phi, uderiv, 1)
-        Vivi.D2!(Dxx_phi, bulk.phi, xderiv, 2)
-        Vivi.D2!(Dyy_phi, bulk.phi, yderiv, 3)
-
-        # set Sd
-        @fastmath @inbounds for j in eachindex(yy)
-            @fastmath @inbounds for i in eachindex(xx)
-                @fastmath @inbounds @simd for a in eachindex(uu)
-                    bulk.Sd[a,i,j] = boundary.Sd[i,j]
-                end
+    # set Sd
+    @fastmath @inbounds for j in eachindex(yy)
+        @fastmath @inbounds for i in eachindex(xx)
+            @fastmath @inbounds @simd for a in eachindex(uu)
+                bulk.Sd[a,i,j] = boundary.Sd[i,j]
             end
         end
-
-
-        # solve for phidg1
-
-        # TODO: parallelize here
-        @fastmath @inbounds for j in eachindex(yy)
-            @fastmath @inbounds for i in eachindex(xx)
-
-                @fastmath @inbounds @simd for a in eachindex(uu)
-                    vars.u       = uu[a]
-                    vars.Sd_d0   = bulk.Sd[a,i,j]
-                    vars.phi_d0  = bulk.phi[a,i,j]
-                    vars.phi_du  = Du_phi[a,i,j]
-                    vars.phi_dxx = Dxx_phi[a,i,j]
-                    vars.phi_dyy = Dyy_phi[a,i,j]
-
-                    phig1_eq_coeff!(ABCS, vars)
-
-                    b_vec[a]     = -ABCS[4]
-
-                    @inbounds @simd for aa in eachindex(uu)
-                        A_mat[a,aa] = ABCS[1] * uderiv.D2[a,aa] + ABCS[2] * uderiv.D[a,aa]
-                    end
-                    A_mat[a,a] += ABCS[3]
-                end
-
-                # boundary condition
-                b_vec[1]    = boundary.phid[i,j]
-                A_mat[1,:] .= 0.0
-                A_mat[1,1]  = 1.0
-
-                sol = view(bulk.phid, :, i, j)
-                solve_lin_system!(sol, A_mat, b_vec)
-            end
-        end
-
-
-        # set Ag1
-        @fastmath @inbounds for j in eachindex(yy)
-            @fastmath @inbounds for i in eachindex(xx)
-                @fastmath @inbounds @simd for a in eachindex(uu)
-                    bulk.A[a,i,j] = boundary.A[i,j]
-                end
-            end
-        end
-
-        nothing
     end
 
+
+    # solve for phidg1
+
+    # TODO: parallelize here
+    @fastmath @inbounds for j in eachindex(yy)
+        @fastmath @inbounds for i in eachindex(xx)
+
+            @fastmath @inbounds @simd for a in eachindex(uu)
+                vars.u       = uu[a]
+                vars.Sd_d0   = bulk.Sd[a,i,j]
+                vars.phi_d0  = bulk.phi[a,i,j]
+                vars.phi_du  = Du_phi[a,i,j]
+                vars.phi_dxx = Dxx_phi[a,i,j]
+                vars.phi_dyy = Dyy_phi[a,i,j]
+
+                phig1_eq_coeff!(ABCS, vars)
+
+                b_vec[a]     = -ABCS[4]
+
+                @inbounds @simd for aa in eachindex(uu)
+                    A_mat[a,aa] = ABCS[1] * uderiv.D2[a,aa] + ABCS[2] * uderiv.D[a,aa]
+                end
+                A_mat[a,a] += ABCS[3]
+            end
+
+            # boundary condition
+            b_vec[1]    = boundary.phid[i,j]
+            A_mat[1,:] .= 0.0
+            A_mat[1,1]  = 1.0
+
+            sol = view(bulk.phid, :, i, j)
+            solve_lin_system!(sol, A_mat, b_vec)
+        end
+    end
+
+
+    # set Ag1
+    @fastmath @inbounds for j in eachindex(yy)
+        @fastmath @inbounds for i in eachindex(xx)
+            @fastmath @inbounds @simd for a in eachindex(uu)
+                bulk.A[a,i,j] = boundary.A[i,j]
+            end
+        end
+    end
+
+    nothing
 end
