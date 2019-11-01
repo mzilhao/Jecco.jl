9968fd1debb92037e3c6f50417a69802d01f1e37
Miguel Zilhao <mzilhao@gmail.com>
2019-11-01 11:05:09 +0000

    KG_3_1: add dphidt to BulkVars struct
    it makes sense to store things this way, i think

--- src/KG_3_1/KG_3_1.jl
+++ src/KG_3_1/KG_3_1.jl
@@ -68,57 +68,63 @@ end
 
 
 struct BulkVars{A}
-    phi  :: A
-    S    :: A
-    Sd   :: A
-    phid :: A
-    A    :: A
+    phi    :: A
+    S      :: A
+    Sd     :: A
+    phid   :: A
+    A      :: A
+    dphidt :: A
 end
-BulkVars(phi, S, Sd, phid, A) =  BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+BulkVars(phi, S, Sd, phid, A, dphidt) =  BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 function BulkVars(phi::Array{<:Number,N}) where {N}
-    S    = similar(phi)
-    Sd   = similar(phi)
-    phid = similar(phi)
-    A    = similar(phi)
-    BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+    S      = similar(phi)
+    Sd     = similar(phi)
+    phid   = similar(phi)
+    A      = similar(phi)
+    dphidt = similar(phi)
+    BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 end
 
 BulkVars(phis::Vector) = [BulkVars(phi) for phi in phis]
 
 function Base.getindex(bulk::BulkVars, i::Int)
-    phi   = bulk.phi[i]
-    S     = bulk.S[i]
-    Sd    = bulk.Sd[i]
-    phid  = bulk.phid[i]
-    A     = bulk.A[i]
-    BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+    phi    = bulk.phi[i]
+    S      = bulk.S[i]
+    Sd     = bulk.Sd[i]
+    phid   = bulk.phid[i]
+    A      = bulk.A[i]
+    dphidt = bulk.dphidt[i]
+    BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 end
 
 function Base.getindex(bulk::BulkVars, kr::AbstractRange)
-    phi   = bulk.phi[kr]
-    S     = bulk.S[kr]
-    Sd    = bulk.Sd[kr]
-    phid  = bulk.phid[kr]
-    A     = bulk.A[kr]
-    BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+    phi    = bulk.phi[kr]
+    S      = bulk.S[kr]
+    Sd     = bulk.Sd[kr]
+    phid   = bulk.phid[kr]
+    A      = bulk.A[kr]
+    dphidt = bulk.dphidt[kr]
+    BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 end
 
 function Base.getindex(bulk::BulkVars, I::Vararg)
-    phi   = bulk.phi[I...]
-    S     = bulk.S[I...]
-    Sd    = bulk.Sd[I...]
-    phid  = bulk.phid[I...]
-    A     = bulk.A[I...]
-    BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+    phi    = bulk.phi[I...]
+    S      = bulk.S[I...]
+    Sd     = bulk.Sd[I...]
+    phid   = bulk.phid[I...]
+    A      = bulk.A[I...]
+    dphidt = bulk.dphidt[I...]
+    BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 end
 
 function Base.getindex(bulk::BulkVars, ::Colon)
-    phi   = bulk.phi[:]
-    S     = bulk.S[:]
-    Sd    = bulk.Sd[:]
-    phid  = bulk.phid[:]
-    A     = bulk.A[:]
-    BulkVars{typeof(phi)}(phi, S, Sd, phid, A)
+    phi    = bulk.phi[:]
+    S      = bulk.S[:]
+    Sd     = bulk.Sd[:]
+    phid   = bulk.phid[:]
+    A      = bulk.A[:]
+    dphidt = bulk.dphidt[:]
+    BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
 end
 
 Base.lastindex(bulk::BulkVars) = lastindex(bulk.phi)
