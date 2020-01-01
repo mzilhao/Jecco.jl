2b0bfeb00420a8187b7331728215d7cac6a42ecc
Miguel Zilhao <mzilhao@gmail.com>
2020-01-01 19:21:56 +0000

    interface->deriv_operators
    renaming

--- src/Jecco.jl
+++ src/Jecco.jl
@@ -1,10 +1,13 @@
 module Jecco
 
+using LinearAlgebra
 using Vivi
 using Parameters
 
-include("interface.jl")
+include("deriv_operators.jl")
 include("output.jl")
 include("KG_3_1/KG_3_1.jl")
+
+export CenteredDiff
 
 end # module
