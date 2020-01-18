96cb8a27b2f825a67fd6527e3558b1094def6b12
Miguel Zilhao <mzilhao@gmail.com>
2020-01-18 14:09:52 +0000

    deriv_operators_test.jl -> deriv_operators_tests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@
 using Jecco
 using Test
 
-@time @testset "derivative operators tests:" begin include("deriv_operators_test.jl") end
+@time @testset "derivative operators tests:" begin include("deriv_operators_tests.jl") end
