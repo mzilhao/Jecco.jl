52f8694ac5f57c78b0af617a589c2eb8a01b477b
Miguel Zilhao <mzilhao@gmail.com>
2020-01-28 21:37:16 +0000

    input: add this file, to read in the hdf5 data

--- src/Jecco.jl
+++ src/Jecco.jl
@@ -8,8 +8,11 @@ export AbstractCoord, Cartesian, GaussLobatto
 export CartCoord, SpectralCoord, Grid, delta
 export CenteredDiff, ChebDeriv
 
+export OpenPMDTimeSeries, get_field
+
 include("deriv_operators.jl")
 include("grid.jl")
+include("input.jl")
 include("output.jl")
 include("spectral.jl")
 include("KG_3_1/KG_3_1.jl")
