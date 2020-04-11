f4f124e4336b2dacd0f753aad86b429554cb751b
Miguel Zilhao <mzilhao@gmail.com>
2020-04-11 10:57:24 +0100

    examples: update 1D PDE example

--- examples/example_1D_PDE.jl
+++ examples/example_1D_PDE.jl
@@ -2,8 +2,30 @@
 using Jecco
 using LinearAlgebra
 using SparseArrays
+using Plots
+
+# return M_ij = x_i A_ij  (no sum in i)
+function mul_col(x::Vector, A::SparseMatrixCSC)
+    @assert size(x)[1] == size(A)[1]
+
+    ii, jj, V = findnz(A)
+    @inbounds for idx in eachindex(V)
+        V[idx] *= x[ii[idx]]
+    end
+    sparse(ii,jj,V)
+end
+
+function build_operator(aa::Vector, Dxx::SparseMatrixCSC,
+                        bb::Vector, Dx::SparseMatrixCSC, cc::Vector)
+    aaDxx = mul_col(aa, Dxx)
+    bbDx  = mul_col(bb, Dx)
+    ccId  = Diagonal(cc)
+
+    aaDxx + bbDx + ccId
+end
 
 source(x) = exp(-x^2) * (-2 + 3*x^2)
+
 
 x_min   = -5.0
 x_max   = 5.0
@@ -25,11 +47,16 @@ Dxx_op = CenteredDiff{1}(2, ord, hx, Nx)
 Dx  = SparseMatrixCSC(Dx_op)
 Dxx = SparseMatrixCSC(Dxx_op)
 
-# TODO: building the operator this way takes longer than performing the LU
-# factorization below. see if there are better ways to do this.
-A_mat = Dxx + x .* Dx + x.^2 .* sparse(I, Nx, Nx)
+aa = ones(Nx)
+bb = copy(x)
+cc = x.^2
+
+# build operator A = Dxx + x Dx + x^2
+A_mat = build_operator(aa, Dxx, bb, Dx, cc)
 b_vec = source.(x)
 
-A_fact = lu(A_mat)
-sol    = zero(b_vec)
-ldiv!(sol, A_fact, b_vec)
+A_fact = factorize(A_mat)
+sol    = A_fact \ b_vec
+
+plot(x, f_exact)
+scatter!(x, sol)
