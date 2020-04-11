43b90c865bbc849ff6fb331acd2e19ba6490d62f
Miguel Zilhao <mzilhao@gmail.com>
2020-04-11 19:01:11 +0100

    examples: improve 2D PDE example

--- examples/example_2D_PDE.jl
+++ examples/example_2D_PDE.jl
@@ -2,25 +2,12 @@
 using Jecco
 using LinearAlgebra
 using SparseArrays
-
-sine2D(x, y, kx, ky) = sin(kx * x) * sin(ky * y)
-
-source1(x, y, kx, ky) = -(kx * kx + ky * ky) * sin(kx * x) * sin(ky * y)
-
-source2(x, y, kx, ky) = 1 + 2 * kx * ky * cos(kx * x) * cos(ky * y) +
-    ky * sin(kx * x) * cos(ky * y) + kx * cos(kx * x) * sin(ky * y) -
-    kx * kx * sin(kx * x) * sin(ky * y) - ky * ky * sin(kx * x) * sin(ky * y)
-
-function getind2D(Nx::Int)
-    (i,j) -> i + (j-1)*Nx
-end
-
+using Plots
+source(x,y) = exp(-x^2 - y^2) * (-4 + 3 * (x^2 + y^2) + 4 * x^2 * y^2)
 
 # https://en.wikipedia.org/wiki/Kronecker_product
 # https://arxiv.org/pdf/1801.01483.pdf
-function build_operators(hx, hy, Nx::Int, Ny::Int)
-    ord = 4
-
+function deriv_operators(hx, hy, Nx::Int, Ny::Int, ord::Int)
     Dx_op  = CenteredDiff{1}(1, ord, hx, Nx)
     Dxx_op = CenteredDiff{1}(2, ord, hx, Nx)
 
@@ -32,7 +19,7 @@ function build_operators(hx, hy, Nx::Int, Ny::Int)
     Dy  = kron(SparseMatrixCSC(Dy_op), I(Nx))
     Dyy = kron(SparseMatrixCSC(Dyy_op), I(Nx))
 
-    Dx, Dy, Dxx, Dyy
+    Dx, Dy, Dxx, Dyy, Dx * Dy
 end
 
 
@@ -45,8 +32,6 @@ y_max            =  5.0
 y_nodes          =  64
 # y_nodes          =  4
 
-nx  = 2
-ny  = 4
 
 xcoord  = CartCoord{1}("x", x_min, x_max, x_nodes, endpoint=false)
 ycoord  = CartCoord{2}("y", y_min, y_max, y_nodes, endpoint=false)
@@ -57,48 +42,47 @@ hy = Jecco.delta(ycoord)
 Nx = xcoord.nodes
 Ny = ycoord.nodes
 
-Dx, Dy, Dxx, Dyy, Dxy = build_operators(hx, hy, Nx, Ny)
+f_exact = [exp(-xcoord[i]^2 - ycoord[j]^2) for i in 1:Nx, j in 1:Ny]
 
 
-kx = 2*pi * nx / (x_max-x_min)
-ky = 2*pi * ny / (y_max-y_min)
+Dx, Dy, Dxx, Dyy, Dxy = deriv_operators(hx, hy, Nx, Ny, ord)
 
-f_exact = [sine2D(xcoord[i], ycoord[j], kx, ky)
-           for i in 1:Nx, j in 1:Ny]
-
-f0 = zeros(Nx,Ny)
-
-# ind2D = getind2D(Nx)
+f0    = zeros(Nx,Ny)
 ind2D = LinearIndices(f0)
 
 M = Nx * Ny
 
-A_mat = spzeros(M,M)
+# A_mat = spzeros(M,M)
 b_vec = zeros(M)
 
-C     = zeros(M)
+axx     = ones(M)
+ayy     = ones(M)
+axy     = zeros(M)
+bx      = zeros(M)
+by      = zeros(M)
+cc      = zeros(M)
+
 
 for j in 1:Ny, i in 1:Nx
-    # idx = ind2D(i,j)
     idx = ind2D[i,j]
 
     xi  = xcoord[i]
     yi  = ycoord[j]
 
-    b_vec[idx] = source1(xi, yi, kx, ky)
-    C[idx] = (kx*kx + ky*ky)
+    axy[idx] = xi * yi
+    bx[idx]  = xi
+    by[idx]  = yi
+    cc[idx]  = xi^2 + yi^2
+
+    b_vec[idx] = source(xi, yi)
 end
 
+# TODO: improve this construction
+A_mat = axx .* Dxx + ayy .* Dyy + axy .* Dxy + bx .* Dx + by .* Dy + Diagonal(cc)
 
 # for kl in 1:M, ij in 1:M
-#     c = C[ij]
-#     A_mat[ij,kl] = Dxx[ij,kl] + Dyy[ij,kl] + Dx[ij,kl] + Dy[ij,kl] + c * I[ij,kl]
+#     A_mat[ij,kl] = Dxx[ij,kl] + Dyy[ij,kl]
 # end
-
-
-for kl in 1:M, ij in 1:M
-    A_mat[ij,kl] = Dxx[ij,kl] + Dyy[ij,kl]
-end
 
 
 # better to do: broadcast!(*, A_mat, Axy, Dxy)
@@ -107,88 +91,16 @@ end
 #     A_mat[ij,kl] = axy * Dxy[ij,kl]
 # end
 
-A_mat = Dxx + Dyy + Dxy + C .* sparse(I, M, M)
 
+A_fact = factorize(A_mat)
+sol    = A_fact \ b_vec
 
-# f_test = similar(f_exact)
-# f_xx   = similar(f_exact)
-# f_yy   = similar(f_exact)
-
-# tmp1 = Dxx * f_exact[:]
-# tmp2 = Dyy * f_exact[:]
-# lap = A_mat * f_exact[:]
-# for idx in eachindex(tmp)
-#     f_xx[idx] = tmp1[idx]
-#     f_yy[idx] = tmp2[idx]
-#     f_test[idx] = lap[idx]
-# end
-
-
-
-
-sol = A_mat \ b_vec
-
-for idx in eachindex(f0)
+@inbounds for idx in eachindex(f0)
     f0[idx] = sol[idx]
 end
 
+j_slice = div(Ny,2)
+x = xcoord[:]
 
-############### FIM #####################
-
-# Axx = ones(Nx,Ny)
-# Axy = ones(Nx,Ny)
-# Ayy = ones(Nx,Ny)
-
-# Bx  = ones(Nx,Ny)
-# By  = ones(Nx,Ny)
-
-# C   = ones(Nx,Ny)
-
-
-# for i in 1:Nx, j in 1:Ny
-#     idx1= ind2D(i,j)
-
-#     axx = Axx[i,j]
-#     axy = Axy[i,j]
-#     ayy = Ayy[i,j]
-#     bx  =  Bx[i,j]
-#     by  =  By[i,j]
-#     c   =   C[i,j]
-
-#     xi = xcoord[i]
-#     yi = ycoord[j]
-
-#     b_vec[idx1] = source2(xi, yi, kx, ky)
-
-#     for ii in 1:Nx, jj in 1:Ny
-#         idx2 = ind2D(ii,jj)
-#         A_mat[idx1,idx2] = axx *  Dxx[idx1,idx2] + 2 * axy * Dx[idx1,idx2] * Dy[idx1,idx2] + ayy * Dyy[idx1,idx2] +
-#             bx * Dx[idx1,idx2] + by * Dy[idx1,idx2] + c * I[idx1,idx2]
-#     end
-# end
-
-
-# sol = A_mat \ b_vec
-
-
-
-# C_mat = zeros(Nx, Nx)
-# # B_mat = zeros(Nx, Nx)
-
-# α  = ones(Nx)
-# # α[1:2:Nx] .= 0.0
-
-# for i in 1:Nx
-#     a0 = α[i]
-#     for j in 1:Nx
-#         C_mat[i,j] = a0 * Dxx[i,j]
-#     end
-#     C_mat[i,i] += 1.0
-# end
-
-
-# # for j in 1:Nx, i in 1:Nx
-# #     a0 = α[i]
-# #     B_mat[i,j] = a0 * Dx[i,j] + 1.0*I[i,j]
-# # end
-
+plot(x, f_exact[:,j_slice])
+scatter!(x, f0[:,j_slice])
