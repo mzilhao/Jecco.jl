41cae4655d217002f1e13fb8f7261d8a9766c0d0
Miguel Zilhao <mzilhao@gmail.com>
2020-04-14 00:21:43 +0100

    KG_3_1: adapt to recent changes

--- src/KG_3_1/system.jl
+++ src/KG_3_1/system.jl
@@ -1,6 +1,8 @@
 
-struct System{G,Du,Dx,Dy}
-    grid   :: G
+struct System{Cu,Cx,Cy,Du,Dx,Dy}
+    ucoord :: Cu
+    xcoord :: Cx
+    ycoord :: Cy
     Du     :: Du
     Duu    :: Du
     Dx     :: Dx
@@ -9,12 +11,9 @@ struct System{G,Du,Dx,Dy}
     Dyy    :: Dy
 end
 
-# FIXME
-function System(ucoord::AbstractCoord,
-                xcoord::AbstractCoord,
-                ycoord::AbstractCoord)
-    grid = Grid(ucoord, xcoord, ycoord)
-
+function System(ucoord::GaussLobattoCoord,
+                xcoord::CartesianCoord,
+                ycoord::CartesianCoord)
     ord = 4
 
     Du  = ChebDeriv{1}(1, ucoord.min, ucoord.max, ucoord.nodes)
@@ -26,18 +25,20 @@ function System(ucoord::AbstractCoord,
     Dy  = CenteredDiff{3}(1, ord, Jecco.delta(ycoord), ycoord.nodes)
     Dyy = CenteredDiff{3}(2, ord, Jecco.delta(ycoord), ycoord.nodes)
 
-    System{typeof(grid), typeof(Du), typeof(Dx),
-           typeof(Dy)}(grid, Du, Duu, Dx, Dxx, Dy, Dyy)
+    System{typeof(ucoord), typeof(xcoord), typeof(ycoord), typeof(Du), typeof(Dx),
+           typeof(Dy)}(ucoord, xcoord, ycoord, Du, Duu, Dx, Dxx, Dy, Dyy)
 end
+
+size(sys::System) = (sys.ucoord.nodes, sys.xcoord.nodes, sys.ycoord.nodes)
 
 function create_systems(p::ParamGrid)
     Nsys = p.udomains
     delta_udom = (p.umax - p.umin) / Nsys
 
-    ucoords = [SpectralCoord{1}("u", p.umin + (i-1)*delta_udom, p.umin + i*delta_udom,
+    ucoords = [GaussLobatto{1}("u", p.umin + (i-1)*delta_udom, p.umin + i*delta_udom,
                                p.unodes) for i in 1:Nsys]
-    xcoord  = CartCoord{2}("x", p.xmin, p.xmax, p.xnodes, endpoint=false)
-    ycoord  = CartCoord{3}("y", p.ymin, p.ymax, p.ynodes, endpoint=false)
+    xcoord  = Cartesian{2}("x", p.xmin, p.xmax, p.xnodes, endpoint=false)
+    ycoord  = Cartesian{3}("y", p.ymin, p.ymax, p.ynodes, endpoint=false)
 
     systems = [System(ucoords[i], xcoord, ycoord) for i in 1:Nsys]
 
