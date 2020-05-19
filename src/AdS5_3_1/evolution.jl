863dc0229044b18070ae4ca7053311096c8de9c6
Miguel Zilhao <mzilhao@gmail.com>
2020-05-19 02:42:59 +0100

    AdS5_3_1 [evolution]: remove module name from function calls

--- src/AdS5_3_1/evolution.jl
+++ src/AdS5_3_1/evolution.jl
@@ -4,13 +4,13 @@ struct EvolTest0 <: AbstractEvolEq end
 function get_f_t!(ff_t, ff, systems, evoleq::EvolTest0)
     Nsys = length(systems)
 
-    boundary  = AdS5_3_1.getboundary(ff)
-    gauge     = AdS5_3_1.getgauge(ff)
-    bulkevols = AdS5_3_1.getbulkevols(ff)
+    boundary    = getboundary(ff)
+    gauge       = getgauge(ff)
+    bulkevols   = getbulkevols(ff)
 
-    boundary_t  = AdS5_3_1.getboundary(ff_t)
-    gauge_t     = AdS5_3_1.getgauge(ff_t)
-    bulkevols_t = AdS5_3_1.getbulkevols(ff_t)
+    boundary_t  = getboundary(ff_t)
+    gauge_t     = getgauge(ff_t)
+    bulkevols_t = getbulkevols(ff_t)
 
     fill!(boundary_t.a4, 0)
     fill!(boundary_t.fx2, 0)
