ab27a710ee8f3ac68ecc1958ee29cf9ce7201ad1
Miguel Zilhao <mzilhao@gmail.com>
2020-05-20 01:33:46 +0100

    tests: add AdS5_3_1 outer equation coeff tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,5 +11,7 @@ using Test
 # AdS5_3_1 inner grid coefficients tests:
 include("AdS5_3_1_equation_inner_coeff.jl")
 
+# AdS5_3_1 outer grid coefficients tests:
+include("AdS5_3_1_equation_outer_coeff.jl")
 
 nothing
