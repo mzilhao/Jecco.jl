492b224b1f11de7565b930f81f25e0bbe060b9a2
Miguel Zilhao <mzilhao@gmail.com>
2020-05-26 00:16:21 +0100

    AdS5_3_1 [evolution]: getting the structure ready

--- src/AdS5_3_1/evolution.jl
+++ src/AdS5_3_1/evolution.jl
@@ -6,105 +6,38 @@ Base.@kwdef struct EvolEq{T,TP<:Potential} <: AbstractEvolEq
     potential     :: TP  = ZeroPotential()
 end
 
-
-function setup_rhs(bulkconstrains, systems::SystemPartition, evoleq::AbstractEvolEq)
-    Nsys = length(systems)
+function setup_rhs(bulkconstrains::BulkPartition{Nsys}, systems::SystemPartition,
+                   evoleq::AbstractEvolEq) where {Nsys}
 
     # function to solve the nested system
-    solve_nested = nested_solver(systems, evoleq)
+    solve_nested! = nested_solver(systems, evoleq)
 
     function (ff_t::EvolVars, ff::EvolVars, t)
-        bulkevols_t = getbulkevolveds(ff_t)
+        bulkevols_t = getbulkevolvedpartition(ff_t)
         boundary_t  = getboundary(ff_t)
         gauge_t     = getgauge(ff_t)
 
-        bulkevols   = getbulkevolveds(ff)
+        bulkevols   = getbulkevolvedpartition(ff)
         boundary    = getboundary(ff)
         gauge       = getgauge(ff)
-
 
         # TODO
         # compute_boundary_t!(boundary_t, bulkevols[1], boundary, gauge, systems[1], evoleq)
 
-        # all bulk variables
-        bulks = Bulk.(bulkevols, bulkconstrains)
-
-        @show typeof(bulks)
-
-        # solve nested system
-        solve_nested(bulks, boundary, gauge)
+        # solve nested system for the constrained variables
+        solve_nested!(bulkconstrains, bulkevols, boundary, gauge)
 
         # TODO
-        # compute_xi_t!(gauge_t, bulks[end], boundary, gauge, systems[end], evoleq)
+        # compute_xi_t!(gauge_t, bulkconstrains[end], bulkevols[end], boundary, gauge, systems[end], evoleq)
 
         for aa in 1:Nsys
             sys = systems[aa]
-            bulk       = bulks[aa]
             bulkevol_t = bulkevols_t[aa]
 
             # TODO
-            # compute_bulk_t!(bulkevol_t, bulks, boundary, gauge, systems, evoleq)
+            # compute_bulkevolved_t!(bulkevol_t, bulkconstrains, bulkevols, boundary, gauge, systems, evoleq)
         end
 
         nothing
     end
-end
-
-
-
-# TODO
-function get_f_t!(evol_t::EvolVars, bulks, boundary::Boundary, gauge::Gauge,
-                  systems::SystemPartition, ::EvolTest0)
-    Nsys = length(systems)
-
-    bulkevols_t = getbulkevolveds(evol_t)
-    boundary_t  = getboundary(evol_t)
-    gauge_t     = getgauge(evol_t)
-
-
-    fill!(boundary_t.a4, 0)
-    fill!(boundary_t.fx2, 0)
-    fill!(boundary_t.fy2, 0)
-    fill!(gauge_t.xi, 0)
-
-    for aa in 1:Nsys
-        sys = systems[aa]
-        bulk       = bulks[aa]
-        bulkevol_t = bulkevols_t[aa]
-
-        fill!(bulkevol_t.B1,  0)
-        fill!(bulkevol_t.B2,  0)
-        fill!(bulkevol_t.G,   0)
-        fill!(bulkevol_t.phi, 0)
-    end
-    nothing
-end
-
-
-# TODO
-function get_f_t!(evol_t::EvolVars, bulks, boundary::Boundary, gauge::Gauge,
-                  systems::SystemPartition, evoleq::EvolEq)
-    Nsys = length(systems)
-
-    bulkevols_t = getbulkevolveds(evol_t)
-    boundary_t  = getboundary(evol_t)
-    gauge_t     = getgauge(evol_t)
-
-
-    fill!(boundary_t.a4, 0)
-    fill!(boundary_t.fx2, 0)
-    fill!(boundary_t.fy2, 0)
-    fill!(gauge_t.xi, 0)
-
-    for aa in 1:Nsys
-        sys = systems[aa]
-        bulk       = bulks[aa]
-        bulkevol_t = bulkevols_t[aa]
-
-        fill!(bulkevol_t.B1,  0)
-        fill!(bulkevol_t.B2,  0)
-        fill!(bulkevol_t.G,   0)
-        fill!(bulkevol_t.phi, 0)
-    end
-    nothing
 end
