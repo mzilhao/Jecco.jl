c8ea8f27d17c61b3f87013156a05773811dce36d
Miguel Zilhao <mzilhao@gmail.com>
2020-05-27 01:07:55 +0100

    AdS5_3_1 [compute_boundary_t]: add actual boundary evolution equations

--- src/AdS5_3_1/compute_bulkevolved_t.jl
+++ src/AdS5_3_1/compute_bulkevolved_t.jl
@@ -14,3 +14,20 @@ function compute_bulkevolved_t!(bulkevol_t::BulkEvolved,
 
     nothing
 end
+
+# TODO
+function compute_bulkevolved_t!(bulkevol_t::BulkEvolved,
+                                bulkconstrain::BulkConstrained, gauge_t::Gauge,
+                                bulkevol::BulkEvolved, boundary::Boundary,
+                                gauge::Gauge, sys::System, evoleq::AffineNull)
+
+    B1_t, B2_t, G_t, phi_t = unpack(bulkevol_t)
+    # B1  , B2  , G  , phi   = unpack(bulkevol)
+
+    fill!(B1_t,  0)
+    fill!(B2_t,  0)
+    fill!(G_t,   0)
+    fill!(phi_t, 0)
+
+    nothing
+end
