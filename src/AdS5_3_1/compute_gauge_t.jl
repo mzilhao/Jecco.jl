f865c6073d1308fedb93a3e988efc9bdbc408d7f
Miguel Zilhao <mzilhao@gmail.com>
2020-05-29 15:28:35 +0100

    AdS5_3_1 [compute_gauge_t]: start to add space here for solving gauge condition

--- src/AdS5_3_1/compute_gauge_t.jl
+++ src/AdS5_3_1/compute_gauge_t.jl
@@ -1,7 +1,14 @@
 
+function compute_xi_t!(gauge_t::Gauge, bulk::BulkConstrained, gauge::Gauge,
+                       sys::System{Outer}, ::ConstantGauge)
+    xi_t = getxi(gauge_t)
+    fill!(xi_t, 0)
+    nothing
+end
+
 # TODO
 function compute_xi_t!(gauge_t::Gauge, bulk::BulkConstrained, gauge::Gauge,
-                       sys::System{Outer}, ::EvolutionEquations)
+                       sys::System{Outer}, gaugecondition::ConstantAH)
     xi_t = getxi(gauge_t)
     fill!(xi_t, 0)
     nothing
