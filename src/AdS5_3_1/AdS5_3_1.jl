3a28a6ac9a0e3b92c1efe3a466681ebfbd6502ee
Miguel Zilhao <mzilhao@gmail.com>
2020-06-07 01:32:02 +0100

    AdS5_3_1: use __init__ to set the number of BLAS to 1 when loading the module
    this saves us from always having to export OMP_NUM_THREADS=1

--- src/AdS5_3_1/AdS5_3_1.jl
+++ src/AdS5_3_1/AdS5_3_1.jl
@@ -29,6 +29,19 @@ include("compute_bulkevolved_t.jl")
 # run the model
 include("run.jl")
 
+# always set the number of BLAS threads to 1 upon loading the module. by default
+# it uses a bunch of them and we don't want that since they trample over each
+# other when solving the nested systems equations. it's much better to thread
+# over the loop. see also the discussion here:
+# https://github.com/JuliaLang/julia/issues/33409
+#
+# this saves us the tedious task of always setting OMP_NUM_THREADS=1 before
+# launching julia.
+function __init__()
+    LinearAlgebra.BLAS.set_num_threads(1)
+    nothing
+end
+
 export SpecCartGrid3D
 export Potential, ZeroPotential
 export BlackBrane, init_data!, init_data
