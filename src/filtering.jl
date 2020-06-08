5be67b6f4a1bcb884c179a8334a82b37824c0c5d
Miguel Zilhao <mzilhao@gmail.com>
2020-06-08 18:19:09 +0100

    filtering: finishing 1D KO filter

--- src/filtering.jl
+++ src/filtering.jl
@@ -15,20 +15,58 @@ function KO_kernel(N::Int, sigma_diss::T) where {T<:Real}
     rmul!(kernel, fac)
 
     # adding one to the middle entry so that it becomes a low-pass filter
-    kernel[N+1] += 1
+    mid = div(length(kernel) + 1, 2)
+    kernel[mid] += 1
 
     kernel
 end
 
 struct ConvFilter{T<:Real,M} <: Filter{T,M}
+    # 'len' is not the length of the kernel, but rather the length of the target vectors
+    len    :: Int
     kernel :: Vector{T}
+    _cache :: Vector{T}
 end
 
 struct KO_Filter{M} end
 
-function KO_Filter{M}(N::Int, sigma_diss::T) where {T<:Real,M}
+function KO_Filter{M}(N::Int, sigma_diss::T, len::Int) where {T<:Real,M}
     kernel = KO_kernel(N, sigma_diss)
-    ConvFilter{T,M}(kernel)
+    _cache = Vector{T}(undef, len)
+    ConvFilter{T,M}(len, kernel, _cache)
 end
 
 KO_Filter(args...) = KO_Filter{1}(args...)
+
+function convolution!(fout::AbstractVector{T}, f::AbstractVector{T},
+                      g::AbstractVector{T}) where {T}
+    f_len = length(f)
+    g_len = length(g)
+    mid   = div(g_len + 1, 2)
+
+    @fastmath @inbounds for i in eachindex(f)
+        sum_i = zero(T)
+
+        if mid <= i <= (f_len - mid + 1)
+            @inbounds for aa in 1:g_len
+                i_circ = i - (mid - aa)
+                sum_i += g[aa] * f[i_circ]
+            end
+        else
+            @inbounds for aa in 1:g_len
+                # imposing periodicity
+                i_circ = 1 + mod(i - (mid-aa) - 1, f_len)
+                sum_i += g[aa] * f[i_circ]
+            end
+        end
+        fout[i] = sum_i
+    end
+
+    fout
+end
+
+function (filter::ConvFilter)(f::AbstractVector)
+    @assert filter.len == length(f)
+    convolution!(filter._cache, f, filter.kernel)
+    copyto!(f, filter._cache)
+end
