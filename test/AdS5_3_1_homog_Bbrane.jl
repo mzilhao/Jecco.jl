be5cfefdf3d8f501dbc898757af10042ab27130c
Thanasis Giannakopoulos <giannakopoulos.th@gmail.com>
2020-07-01 14:29:22 +0100

    updating AdS5_3_1 homogeneous BB test to try and fix it from failing on mac.
    The error before was that A_in, _out etc are NaN. Now we define them
    as copies from the bulk fields.
    
    Signed-off-by: Thanasis Giannakopoulos <giannakopoulos.th@gmail.com>

--- test/AdS5_3_1_homog_Bbrane.jl
+++ test/AdS5_3_1_homog_Bbrane.jl
@@ -56,7 +56,7 @@ using Jecco.AdS5_3_1
     #################
 
     # gauge
-    xi     = gauge.xi[1,1,1] #due to homogeneity; change for non-homog.
+    xi     = copy(gauge.xi[1,1,1]) #due to homogeneity; change for non-homog.
     Du_in  = systems[1].Du
     Du_out = systems[2].Du
 
@@ -66,10 +66,10 @@ using Jecco.AdS5_3_1
     bulk_in = Bulk(bulkevols[i], bulkconstrains[i])
 
     chart_in = atlas.charts[i]
-    uu_in    = chart_in.coords[1][:]
+    uu_in    = copy(chart_in.coords[1][:])
 
-    A_in  = bulk_in.A[:,1,1];
-    Au_in = Du_in*A_in;
+    A_in  = copy(bulk_in.A[:,1,1]);
+    Au_in = copy(Du_in*A_in);
 
     # outer grids
     num_out_domains = grid.u_outer_domains
@@ -93,12 +93,13 @@ using Jecco.AdS5_3_1
         chart_out_i = atlas.charts[i]
         append!(chart_out, [chart_out_i])
 
-        uu_out_i    = chart_out_i.coords[1][:]
+        uu_out_i    = copy(chart_out_i.coords[1][:])
         append!(uu_out, [uu_out_i])
 
-        A_out_i = bulk_out_i.A[:,1,1]
+        A_out_i  = copy(bulk_out_i.A[:,1,1])
+        Au_out_i = copy(Du_out*A_out_i)
         append!(A_out,  [A_out_i])
-        append!(Au_out, [Du_out*A_out_i])
+        append!(Au_out, [Au_out_i])
 
     end
 
@@ -106,8 +107,8 @@ using Jecco.AdS5_3_1
     # Compare value of A on the interface of domains#
     #################################################
 
-    A_in_end = (1/(uu_in[end]^2) + 2.0*xi/uu_in[end] +   xi^2 + uu_in[end]^2 * A_in[end])
-    Au_in_end = (-2/(uu_in[end]^3) - 2.0*xi/(uu_in[end]^2)  + 2.0* uu_in[end]* A_in[end] + (uu_in[end]^2)* Au_in[end])
+    A_in_end  = copy( (1/(uu_in[end]^2) + 2.0*xi/uu_in[end] +   xi^2 + uu_in[end]^2 * A_in[end]) )
+    Au_in_end = copy( (-2/(uu_in[end]^3) - 2.0*xi/(uu_in[end]^2)  + 2.0* uu_in[end]* A_in[end] + (uu_in[end]^2)* Au_in[end]) )
 
     # in-out interface
     @test A_in_end  ≈ A_out[1][1]
@@ -127,30 +128,33 @@ using Jecco.AdS5_3_1
     #######################################################
 
     # comparison inner grid only
-    A_in_reg_num = zeros(length(A_in))
-    A_in_reg_exact = zeros(length(A_in))
-    Au_in_reg_num = zeros(length(A_in))
+    A_in_reg_num    = zeros(length(A_in))
+    A_in_reg_exact  = zeros(length(A_in))
+    Au_in_reg_num   = zeros(length(A_in))
     Au_in_reg_exact = zeros(length(A_in))
+
     a4 = -id.energy_dens/0.75
 
     # A regularized inner grid
     for i in 1:length(A_in)
-        u = uu_in[i]
-        A = A_in[i]
+        u  = uu_in[i]
+        A  = A_in[i]
         Au = Au_in[i]
+
         A_in_reg_num[i]    = xi^2 + u^2 * A
         Au_in_reg_num[i]   = 2.0*u * A + u^2 * Au
         A_in_reg_exact[i]  = xi^2 + u^2 * a4 /(1.0 + 2.0* xi *u + xi^2 *u^2 )
-        Au_in_reg_exact[i] =  (2.0 * u * a4)/((1.0 + xi *u)^3)
+        Au_in_reg_exact[i] = (2.0 * u * a4)/((1.0 + xi *u)^3)
     end
 
     @test A_in_reg_num  ≈ A_in_reg_exact
     @test Au_in_reg_num ≈ Au_in_reg_exact
 
     # comparison outer grids only
-    uu_out_full    = []
-    A_out_full_num = []
+    uu_out_full     = []
+    A_out_full_num  = []
     Au_out_full_num = []
+    
     for i in 1:num_out_domains
         append!(uu_out_full, uu_out[i])
         append!(A_out_full_num, A_out[i])
@@ -158,11 +162,12 @@ using Jecco.AdS5_3_1
     end
 
     #exact
-    A_out_full_exact = zeros(length(A_out_full_num))
+    A_out_full_exact  = zeros(length(A_out_full_num))
     Au_out_full_exact = zeros(length(A_out_full_num))
+
     for i in 1:length(A_out_full_num)
         u = uu_out_full[i]
-        A_out_full_exact[i] = 1/(u^2) + 2.0*xi/u + xi^2 + u^2 * a4/(1.0 + 2.0* xi *u + xi^2 *u^2)
+        A_out_full_exact[i]  = 1/(u^2) + 2.0*xi/u + xi^2 + u^2 * a4/(1.0 + 2.0* xi *u + xi^2 *u^2)
         Au_out_full_exact[i] = -2/(u^3) - 2.0*xi/(u^2)  - u^2 * a4 * (2*xi + 2*u*xi)/((1.0 + 2.0* xi *u + xi^2 *u^2)^2) + 2*a4*u/(1.0 + 2.0* xi *u + xi^2 *u^2)
     end
 
