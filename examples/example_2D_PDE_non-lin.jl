46d80a6457c9e5c1ca3b6c69a5c60d3226b329ed
Miguel Zilhao <mzilhao@gmail.com>
2020-08-05 18:37:56 +0100

    examples: continue along with 2D non-linear PDE

--- examples/example_2D_PDE_non-lin.jl
+++ examples/example_2D_PDE_non-lin.jl
@@ -6,29 +6,40 @@ using Plots
 
 #= solving equation
 
-f_xx + f_yy + x y f_xy + x f_x + y f_y + (x^2 + y^2) f
-  - x/2 (f_x)^2 - y/2 (f_y)^2 + f_x f_y = Source(x,y)
+ f_xx + f_yy + x y f_xy + x f_x + y f_y + (x^2 + y^2) f
+  + (f_x)^2 + (f_y)^2 + x y f_x f_y = Source(x,y)
 
 for the source below, the solution should be given by
 
-f = exp(-x^2 - y^2)
-
+ f = exp(-x^2 - y^2)
 =#
 
-source(x,y) = exp(-x^2 - y^2) * (-4 + 3 * (x^2 + y^2) + 4 * x^2 * y^2) -
-    2 * exp(-2*x^2 - 2*y^2) * (x^3 + y^3 - 2 * x * y)
+source(x,y) = exp(-x^2 - y^2) * (-4 + 3 * (x^2 + y^2) + 4 * x^2 * y^2) +
+    4 * exp(-2*x^2 - 2*y^2) * (x^2 + y^2 + x^2 * y^2)
 
+function compute_residual!(res::Array, f::Array, x::Vector, y::Vector,
+                           Dxx_op::Jecco.FiniteDiffDeriv, Dyy_op::Jecco.FiniteDiffDeriv,
+                           Dx_op::Jecco.FiniteDiffDeriv, Dy_op::Jecco.FiniteDiffDeriv)
+    Nx = length(x)
+    Ny = length(y)
 
-function compute_residual(Dxx::SparseMatrixCSC, Dyy::SparseMatrixCSC, Dxy::SparseMatrixCSC,
-                          Dx::SparseMatrixCSC, Dy::SparseMatrixCSC)
-    # Jecco.mul_col!(axx, Dxx)
-    # Jecco.mul_col!(ayy, Dyy)
-    # Jecco.mul_col!(axy, Dxy)
-    # Jecco.mul_col!(bx,  Dx)
-    # Jecco.mul_col!(by,  Dy)
-    # ccId = Diagonal(cc)
+    f_xx = Dxx_op * f
+    f_yy = Dyy_op * f
+    f_x  = Dx_op  * f
+    f_y  = Dy_op  * f
+    f_xy = Dx_op  * f_y
 
-    # Dxx + Dyy + Dxy + Dx + Dy + ccId
+    for j in 1:Ny, i in 1:Nx
+        x1 = x[i]
+        y1 = y[j]
+
+        res[i,j] = f_xx[i,j] + f_yy[i,j] + x1 * y1 * f_xy[i,j] +
+            x1 * f_x[i,j] + y1 * f_y[i,j] +
+            (x1*x1 + y1*y1) * f[i,j] + f_x[i,j]^2 + f_y[i,j]^2 +
+            x1 * y1 * f_x[i,j] * f_y[i,j] - source(x1,y1)
+    end
+
+    res
 end
 
 
@@ -56,7 +67,7 @@ function deriv_operators(hx, hy, Nx::Int, Ny::Int, ord::Int)
     Dy_op  = CenteredDiff{2}(1, ord, hy, Ny)
     Dyy_op = CenteredDiff{2}(2, ord, hy, Ny)
 
-    Dx_op, Dy_op, Dxx_op, Dyy_op
+    Dxx_op, Dyy_op, Dx_op, Dy_op
 end
 
 #=
@@ -67,25 +78,28 @@ from the 1-dimensional ones. see for instance:
 
   https://arxiv.org/pdf/1801.01483.pdf (section 5)
 =#
-function deriv_matrices(Dx_op, Dy_op, Dxx_op, Dyy_op)
+function deriv_matrices(Dxx_op::Jecco.FiniteDiffDeriv, Dyy_op::Jecco.FiniteDiffDeriv,
+                        Dx_op::Jecco.FiniteDiffDeriv, Dy_op::Jecco.FiniteDiffDeriv)
     Dx  = kron(I(Ny), SparseMatrixCSC(Dx_op))
     Dxx = kron(I(Ny), SparseMatrixCSC(Dxx_op))
     Dy  = kron(SparseMatrixCSC(Dy_op), I(Nx))
     Dyy = kron(SparseMatrixCSC(Dyy_op), I(Nx))
 
-    Dx, Dy, Dxx, Dyy, Dx * Dy
+    Dxx, Dyy, Dx * Dy, Dx, Dy
 end
 
 
 x_min    = -5.0
 x_max    =  5.0
 x_nodes  =  128
+# x_nodes  =  1024
 y_min    = -5.0
 y_max    =  5.0
 y_nodes  =  64
+# y_nodes  =  1024
 
-# ord = 4
-ord = 2
+ord = 4
+# ord = 2
 
 xcoord  = Cartesian{1}("x", x_min, x_max, x_nodes, endpoint=false)
 ycoord  = Cartesian{2}("y", y_min, y_max, y_nodes, endpoint=false)
@@ -99,11 +113,18 @@ Ny = ycoord.nodes
 f_exact = [exp(-xcoord[i]^2 - ycoord[j]^2) for i in 1:Nx, j in 1:Ny]
 
 
-Dx_op, Dy_op, Dxx_op, Dyy_op = deriv_operators(hx, hy, Nx, Ny, ord)
-Dx, Dy, Dxx, Dyy, Dxy        = deriv_matrices(Dx_op, Dy_op, Dxx_op, Dyy_op)
+Dxx_op, Dyy_op, Dx_op, Dy_op = deriv_operators(hx, hy, Nx, Ny, ord)
+Dxx, Dyy, Dxy, Dx, Dy        = deriv_matrices(Dxx_op, Dyy_op, Dx_op, Dy_op)
 
 fsol   = zeros(Nx,Ny)
 ind2D  = LinearIndices(fsol)
+
+fsol = copy(f_exact)
+
+res = similar(fsol)
+
+compute_residual!(res, fsol, xcoord[:], ycoord[:], Dxx_op, Dyy_op, Dx_op, Dy_op)
+
 
 M = Nx * Ny
 
@@ -122,6 +143,7 @@ f0      = f_exact[:]
 f0_x = Dx * f0
 f0_y = Dy * f0
 
+# FIXME
 for j in 1:Ny, i in 1:Nx
     idx = ind2D[i,j]
 
@@ -136,6 +158,7 @@ for j in 1:Ny, i in 1:Nx
     b_vec[idx] = source(xi, yi)
 end
 
+# FIXME
 # build (linearized) operator A = Dxx + Dyy + x y Dxy + f_y(0) Dx + f_x(0) Dy + (x^2 + y^2)
 A_mat = build_operator(Dxx, Dyy, Dxy, Dx, Dy, axx, ayy, axy, bx, by, cc)
 
