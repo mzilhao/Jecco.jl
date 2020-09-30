46720ec20dfd93a00241c0256bc711bb2432afa1
Miguel Zilhao <miguel.zilhao.nogueira@tecnico.ulisboa.pt>
2020-09-30 17:19:39 +0100

    AdS5_3_1 [utils]: add structures and methods to read in all variables from a folder with the hdf5 data
    these are specific for the variables used in this module, and is useful for
    postprocessing

--- src/AdS5_3_1/utils.jl
+++ src/AdS5_3_1/utils.jl
@@ -1,5 +1,170 @@
 using HDF5
 
+"""
+Need to have method `get_data`
+"""
+abstract type TimeSeries{N,T} end
+
+struct BoundaryTimeSeries{N,T} <: TimeSeries{N,T}
+    ts    :: T
+    field :: Symbol
+
+    function BoundaryTimeSeries(foldername::String, field::Symbol)
+        ts = OpenPMDTimeSeries(foldername, "boundary_")
+        new{3,typeof(ts)}(ts, field)
+    end
+end
+
+struct XiTimeSeries{N,T} <: TimeSeries{N,T}
+    ts :: T
+
+    function XiTimeSeries(foldername::String)
+        ts = OpenPMDTimeSeries(foldername, "gauge_")
+        new{3,typeof(ts)}(ts)
+    end
+end
+
+struct BulkTimeSeries{N,T} <: TimeSeries{N,T}
+    ts        :: T
+    field     :: Symbol
+    component :: Int
+
+    function BulkTimeSeries(foldername::String, field::Symbol, c::Int)
+        ts = OpenPMDTimeSeries(foldername, "bulk_")
+        new{3,typeof(ts)}(ts, field, c)
+    end
+end
+
+struct VEVTimeSeries{N,T} <: TimeSeries{N,T}
+    ts  :: T
+    vev :: Symbol
+
+    function VEVTimeSeries(foldername::String, vev::Symbol)
+        ts = OpenPMDTimeSeries(foldername, "boundary_")
+        new{3,typeof(ts)}(ts, vev)
+    end
+end
+
+
+function get_data(ff::BoundaryTimeSeries; it=Int, verbose::Bool=false)
+    get_field(ff.ts, it=it, verbose=verbose, field=String(ff.field))
+end
+
+function get_data(xi::XiTimeSeries; it=Int, verbose::Bool=false)
+    get_field(xi.ts, it=it, verbose=verbose, field="xi")
+end
+
+function get_data(ff::BulkTimeSeries; it=Int, verbose::Bool=false)
+    field = "$(ff.field) c=$(ff.component)"
+    get_field(ff.ts, it=it, verbose=verbose, field=field)
+end
+
+function get_data(ff::VEVTimeSeries; it=Int, verbose::Bool=false)
+    if ff.vev == :energy
+        return get_energy(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :Jx
+        return get_Jx(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :Jy
+        return get_Jy(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :px
+        return get_px(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :py
+        return get_py(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :pz
+        return get_pz(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :Jx
+        return get_Jx(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :pxy
+        return get_pxy(ff.ts, it=it, verbose=verbose)
+    elseif ff.vev == :Ophi
+        return get_Ophi(ff.ts, it=it, verbose=verbose)
+    else
+        error("Unknown VEV")
+    end
+end
+
+
+function Base.getindex(ff::TimeSeries, a::Int, idx::Vararg)
+    it = ff.ts.iterations[a]
+    f, = get_data(ff, it=it)
+    f[idx...]
+end
+
+function Base.getindex(ff::TimeSeries, aa::UnitRange, idx::Vararg)
+    it  = ff.ts.iterations[aa[1]]
+    f0, = get_data(ff, it=it)
+
+    Na    = length(aa)
+    size_ = size(f0[idx...])
+    f     = zeros(Na, size_...)
+
+    slicer = [Colon() for _ in 1:ndims(f0)]
+    for a in aa
+        it  = ff.ts.iterations[a]
+        f0, = get_data(ff, it=it)
+        f[a,slicer...] .= f0[idx...]
+    end
+    f
+end
+
+function Base.getindex(ff::TimeSeries, ::Colon, idx::Vararg)
+    it  = ff.ts.iterations[1]
+    f0, = get_data(ff, it=it)
+
+    Na    = length(ff.ts.iterations)
+    size_ = size(f0[idx...])
+    f     = zeros(Na, size_...)
+
+    slicer = [Colon() for _ in 1:ndims(f0)]
+    for a in 1:Na
+        it  = ff.ts.iterations[a]
+        f0, = get_data(ff, it=it)
+        f[a,slicer...] .= f0[idx...]
+    end
+    f
+end
+
+
+function Jecco.get_coords(ff::TimeSeries, a::Int, idx::Vararg)
+    it = ff.ts.iterations[a]
+    f, chart = get_data(ff, it=it)
+    t = ff.ts.current_t
+    t, chart[idx...]...
+end
+
+function Jecco.get_coords(ff::TimeSeries, aa::UnitRange, idx::Vararg)
+    it = ff.ts.iterations[aa[1]]
+    f0, chart = get_data(ff, it=it)
+    t0 = ff.ts.current_t
+
+    Na = length(aa)
+    t  = Vector{typeof(t0)}(undef, Na)
+    for a in aa
+        it   = ff.ts.iterations[a]
+        get_data(ff, it=it)
+        t0   = ff.ts.current_t
+        t[a] = t0
+    end
+    t, chart[idx...]...
+end
+
+function Jecco.get_coords(ff::TimeSeries, ::Colon, idx::Vararg)
+    it = ff.ts.iterations[1]
+    f0, chart = get_data(ff, it=it)
+    t0 = ff.ts.current_t
+
+    Na = length(ff.ts.iterations)
+    t  = Vector{typeof(t0)}(undef, Na)
+    for a in 1:Na
+        it = ff.ts.iterations[a]
+        get_data(ff, it=it)
+        t0 = ff.ts.current_t
+        t[a] = t0
+    end
+    t, chart[idx...]...
+end
+
+
 function convert_to_mathematica(dirname::String; outfile::String="data_mathematica.h5")
     ts = OpenPMDTimeSeries(dirname, prefix="boundary_")
 
