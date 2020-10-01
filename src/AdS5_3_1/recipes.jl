3bdb8939c131551839735a30e329b480db8ebc2f
Miguel Zilhao <mzilhao@gmail.com>
2020-10-01 17:50:18 +0100

    AdS5_3_1 [recipes]: change the recipes to a more manual approach
    i'm not too happy about these, but i think i'll leave them for now

--- src/AdS5_3_1/recipes.jl
+++ src/AdS5_3_1/recipes.jl
@@ -1,68 +1,30 @@
 using RecipesBase
-using Printf
 
-@recipe function plot(f_ts::TimeSeries{3}, i0, i1, i2, i3)
-    f = f_ts[i0,i1,i2,i3]
-    coord = get_coords(f_ts, i0,i1,i2,i3)
+@recipe function plot(f_ts::TimeSeries{2}, i0::Union{Colon, UnitRange}, i1::Int, i2::Int)
+    xguide := "t"
+    tt, xx, yy = get_coords(f_ts,i0,i1,i2)
+    f = f_ts[i0,i1,i2]
+    tt, f
+end
 
-    ix_arr = isa.([i0,i1,i2,i3], Union{Colon,UnitRange})
-
-    if sum(ix_arr) != 1
-        error("Need to have a 1-D array...")
-    end
-
-    # direction along which we're plotting
-    ix = argmin(abs.(ix_arr .- 1))
-    xx = coord[ix]
-
-    if ix == 1
-        xguide := "t"
-
-        xc = "x =  $(@sprintf("%.2f", coord[3]))"
-        yc = ", y =  $(@sprintf("%.2f", coord[4]))"
-
-        if !isnan(coord[2])
-            uc = ", u =  $(@sprintf("%.2f", coord[2]))"
-        else
-            uc = ""
-        end
-        label --> xc * yc * uc
-
-    elseif ix == 2
-        xguide := "u"
-
-        tc = "t =  $(@sprintf("%.2f", coord[1]))"
-        xc = ", x =  $(@sprintf("%.2f", coord[3]))"
-        yc = ", y =  $(@sprintf("%.2f", coord[4]))"
-
-        label --> tc * xc * yc
-
-    elseif ix == 3
-        xguide := "x"
-
-        tc = "t =  $(@sprintf("%.2f", coord[1]))"
-        yc = ", y =  $(@sprintf("%.2f", coord[4]))"
-
-        if !isnan(coord[2])
-            uc = ", u =  $(@sprintf("%.2f", coord[2]))"
-        else
-            uc = ""
-        end
-        label --> tc * yc * uc
-
-    elseif ix == 4
-        xguide := "y"
-
-        tc = "t =  $(@sprintf("%.2f", coord[1]))"
-        xc = ", x =  $(@sprintf("%.2f", coord[3]))"
-
-        if !isnan(coord[2])
-            uc = ", u =  $(@sprintf("%.2f", coord[2]))"
-        else
-            uc = ""
-        end
-        label --> tc * yc * uc
-    end
-
+@recipe function plot(f_ts::TimeSeries{2}, i0::Int, i1::Union{Colon, UnitRange}, i2::Int)
+    xguide := "x"
+    tt, xx, yy = get_coords(f_ts,i0,i1,i2)
+    f = f_ts[i0,i1,i2]
     xx, f
+end
+
+@recipe function plot(f_ts::TimeSeries{2}, i0::Int, i1::Int, i2::Union{Colon, UnitRange})
+    xguide := "y"
+    tt, xx, yy = get_coords(f_ts,i0,i1,i2)
+    f = f_ts[i0,i1,i2]
+    yy, f
+end
+
+@recipe function plot(f_ts::TimeSeries{2}, i0::Int, i1::Union{Colon, UnitRange}, i2::Union{Colon, UnitRange})
+    xguide := "y"
+    yguide := "x"
+    tt, xx, yy = get_coords(f_ts,i0,i1,i2)
+    f = f_ts[i0,i1,i2]
+    yy, xx, f
 end
