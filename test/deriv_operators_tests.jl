eb334750c667c35759b2e0667105027f512a5443
Miguel Zilhao <mzilhao@gmail.com>
2020-10-23 15:00:23 +0100

    tests [deriv_operators_tests]: prefix calculate_weights call with "Jecco."
    this is needed since the function is not exported

--- test/deriv_operators_tests.jl
+++ test/deriv_operators_tests.jl
@@ -1,114 +1,110 @@
-# TG: if I do not add the line below it seems that the Fornberg
-# func. is not loaded with Jecco for me locally in the FD_new branch
-
-include("../src/fornberg.jl")
 
 @testset "Fornberg FD weights tests:" begin
 
     # all stencils provide derivatives at point labelled with 0
 
     # centered 1st order derivatives i.e. ...,-1,0,1,...
-    @test calculate_weights(1,2,1) == [-1//2, 0//1, 1//2]
-    @test calculate_weights(1,4,2) == [1//12, -2//3, 0//1, 2//3, -1//12]
-    @test calculate_weights(1,6,3) == [-1//60, 3//20, -3//4, 0//1, 3//4, -3//20, 1//60]
-    @test calculate_weights(1,8,4) == [1//280, -4//105, 1//5, -4//5, 0//1,
+    @test Jecco.calculate_weights(1,2,1) == [-1//2, 0//1, 1//2]
+    @test Jecco.calculate_weights(1,4,2) == [1//12, -2//3, 0//1, 2//3, -1//12]
+    @test Jecco.calculate_weights(1,6,3) == [-1//60, 3//20, -3//4, 0//1, 3//4, -3//20, 1//60]
+    @test Jecco.calculate_weights(1,8,4) == [1//280, -4//105, 1//5, -4//5, 0//1,
                                        4//5, -1//5, 4//105, -1//280 ]
 
     # forward 1st order derivatives i.e. 0,1,...
-    @test calculate_weights(1,1,0) == [-1//1, 1//1]
-    @test calculate_weights(1,2,0) == [-3//2, 2//1, -1//2]
-    @test calculate_weights(1,3,0) == [-11//6, 3//1, -3//2, 1//3]
-    @test calculate_weights(1,4,0) == [-25//12, 4//1, -3//1, 4//3, -1//4]
-    @test calculate_weights(1,5,0) == [-137//60, 5//1, -5//1, 10//3, -5//4, 1//5]
-    @test calculate_weights(1,6,0) == [-49//20, 6//1, -15//2, 20//3, -15//4, 6//5, -1//6]
+    @test Jecco.calculate_weights(1,1,0) == [-1//1, 1//1]
+    @test Jecco.calculate_weights(1,2,0) == [-3//2, 2//1, -1//2]
+    @test Jecco.calculate_weights(1,3,0) == [-11//6, 3//1, -3//2, 1//3]
+    @test Jecco.calculate_weights(1,4,0) == [-25//12, 4//1, -3//1, 4//3, -1//4]
+    @test Jecco.calculate_weights(1,5,0) == [-137//60, 5//1, -5//1, 10//3, -5//4, 1//5]
+    @test Jecco.calculate_weights(1,6,0) == [-49//20, 6//1, -15//2, 20//3, -15//4, 6//5, -1//6]
 
     # forward 1st order derivatives i.e. -1,0,1,2,...
-    @test calculate_weights(1,3,1) == [-1//3, -1//2, 1//1, -1//6]
-    @test calculate_weights(1,4,1) == [-1//4, -5//6, 3//2, -1//2, 1//12]
-    @test calculate_weights(1,5,1) == [-1//5, -13//12, 2//1, -1//1, 1//3, -1//20]
-    @test calculate_weights(1,6,1) == [-1//6, -77//60, 5//2, -5//3, 5//6, -1//4, 1//30]
-    @test calculate_weights(1,7,1) == [-1//7, -29//20, 3//1, -5//2, 5//3, -3//4, 1//5, -1//42]
+    @test Jecco.calculate_weights(1,3,1) == [-1//3, -1//2, 1//1, -1//6]
+    @test Jecco.calculate_weights(1,4,1) == [-1//4, -5//6, 3//2, -1//2, 1//12]
+    @test Jecco.calculate_weights(1,5,1) == [-1//5, -13//12, 2//1, -1//1, 1//3, -1//20]
+    @test Jecco.calculate_weights(1,6,1) == [-1//6, -77//60, 5//2, -5//3, 5//6, -1//4, 1//30]
+    @test Jecco.calculate_weights(1,7,1) == [-1//7, -29//20, 3//1, -5//2, 5//3, -3//4, 1//5, -1//42]
 
     # forward 1st order derivatives i.e. -2,-1,0,1,2,3,...
-    @test calculate_weights(1,5,2) == [1//20, -1//2, -1//3, 1//1, -1//4, 1//30]
-    @test calculate_weights(1,6,2) == [1//30, -2//5, -7//12, 4//3, -1//2, 2//15, -1//60]
+    @test Jecco.calculate_weights(1,5,2) == [1//20, -1//2, -1//3, 1//1, -1//4, 1//30]
+    @test Jecco.calculate_weights(1,6,2) == [1//30, -2//5, -7//12, 4//3, -1//2, 2//15, -1//60]
 
     # backward 1st order derivatives i.e. ...,-1,0
-    @test calculate_weights(1,1,1) == [-1//1, 1//1]
-    @test calculate_weights(1,2,2) == [1//2, -2//1, 3//2]
-    @test calculate_weights(1,3,3) == [-1//3, 3//2, -3//1, 11//6]
-    @test calculate_weights(1,4,4) == [1//4, -4//3, 3//1, -4//1, 25//12]
+    @test Jecco.calculate_weights(1,1,1) == [-1//1, 1//1]
+    @test Jecco.calculate_weights(1,2,2) == [1//2, -2//1, 3//2]
+    @test Jecco.calculate_weights(1,3,3) == [-1//3, 3//2, -3//1, 11//6]
+    @test Jecco.calculate_weights(1,4,4) == [1//4, -4//3, 3//1, -4//1, 25//12]
 
     # backward 1st order derivatives i.e. ...,-2,-1,0,1
-    @test calculate_weights(1,3,2) == [1//6, -1//1, 1//2, 1//3]
-    @test calculate_weights(1,4,3) == [-1//12, 1//2, -3//2, 5//6, 1//4]
-    @test calculate_weights(1,5,4) == [1//20, -1//3, 1//1, -2//1, 13//12, 1//5]
+    @test Jecco.calculate_weights(1,3,2) == [1//6, -1//1, 1//2, 1//3]
+    @test Jecco.calculate_weights(1,4,3) == [-1//12, 1//2, -3//2, 5//6, 1//4]
+    @test Jecco.calculate_weights(1,5,4) == [1//20, -1//3, 1//1, -2//1, 13//12, 1//5]
 
     # backward 1st order derivatives i.e. ...,-3,-2,-1,0,1,2
-    @test calculate_weights(1,5,3) == [-1//30, 1//4, -1//1, 1//3, 1//2, -1//20]
-    @test calculate_weights(1,6,4) == [1//60, -2//15, 1//2, -4//3, 7//12, 2//5, -1//30]
+    @test Jecco.calculate_weights(1,5,3) == [-1//30, 1//4, -1//1, 1//3, 1//2, -1//20]
+    @test Jecco.calculate_weights(1,6,4) == [1//60, -2//15, 1//2, -4//3, 7//12, 2//5, -1//30]
 
     # centered 2nd order derivatives i.e. ...,-1,0,1,...
-    @test calculate_weights(2,2,1) == [1//1, -2//1, 1//1]
-    @test calculate_weights(2,4,2) == [-1//12, 4//3, -5//2, 4//3, -1//12]
-    @test calculate_weights(2,6,3) == [1//90, -3//20, 3//2, -49//18,
+    @test Jecco.calculate_weights(2,2,1) == [1//1, -2//1, 1//1]
+    @test Jecco.calculate_weights(2,4,2) == [-1//12, 4//3, -5//2, 4//3, -1//12]
+    @test Jecco.calculate_weights(2,6,3) == [1//90, -3//20, 3//2, -49//18,
                                        3//2, -3//20, 1//90]
-    @test calculate_weights(2,8,4) == [-1//560, 8//315, -1//5, 8//5, -205//72,
+    @test Jecco.calculate_weights(2,8,4) == [-1//560, 8//315, -1//5, 8//5, -205//72,
                                        8//5, -1//5, 8//315, -1//560]
 
     # forward 2nd order derivatives i.e. 0,1,...
-    @test calculate_weights(2,2,0) == [1//1, -2//1, 1//1]
-    @test calculate_weights(2,3,0) == [2//1, -5//1, 4//1, -1//1]
-    @test calculate_weights(2,4,0) == [35//12, -26//3, 19//2, -14//3, 11//12]
-    @test calculate_weights(2,5,0) == [15//4, -77//6, 107//6, -13//1, 61//12, -5//6]
-    @test calculate_weights(2,6,0) == [203//45, -87//5, 117//4, -254//9, 33//2, -27//5, 137//180]
-    @test calculate_weights(2,7,0) == [469//90, -223//10, 879//20, -949//18, 41//1, -201//10,
+    @test Jecco.calculate_weights(2,2,0) == [1//1, -2//1, 1//1]
+    @test Jecco.calculate_weights(2,3,0) == [2//1, -5//1, 4//1, -1//1]
+    @test Jecco.calculate_weights(2,4,0) == [35//12, -26//3, 19//2, -14//3, 11//12]
+    @test Jecco.calculate_weights(2,5,0) == [15//4, -77//6, 107//6, -13//1, 61//12, -5//6]
+    @test Jecco.calculate_weights(2,6,0) == [203//45, -87//5, 117//4, -254//9, 33//2, -27//5, 137//180]
+    @test Jecco.calculate_weights(2,7,0) == [469//90, -223//10, 879//20, -949//18, 41//1, -201//10,
                                        1019//180, -7//10]
 
     # forward 2nd order derivatives i.e. -1,0,1,2,...
-    @test calculate_weights(2,3,1) == [1//1, -2//1, 1//1, 0//1]
-    @test calculate_weights(2,4,1) == [11//12, -5//3, 1//2, 1//3, -1//12]
-    @test calculate_weights(2,5,1) == [5//6, -5//4, -1//3, 7//6, -1//2, 1//12]
-    @test calculate_weights(2,6,1) == [137//180, -49//60, -17//12, 47//18, -19//12,
+    @test Jecco.calculate_weights(2,3,1) == [1//1, -2//1, 1//1, 0//1]
+    @test Jecco.calculate_weights(2,4,1) == [11//12, -5//3, 1//2, 1//3, -1//12]
+    @test Jecco.calculate_weights(2,5,1) == [5//6, -5//4, -1//3, 7//6, -1//2, 1//12]
+    @test Jecco.calculate_weights(2,6,1) == [137//180, -49//60, -17//12, 47//18, -19//12,
                                        31//60, -13//180]
 
     # forward 2nd order derivatives i.e. -2,-1,0,1,2,3,...
-    @test calculate_weights(2,5,2) == [-1//12, 4//3, -5//2, 4//3, -1//12, 0//1]
-    @test calculate_weights(2,6,2) == [-13//180, 19//15, -7//3, 10//9,
+    @test Jecco.calculate_weights(2,5,2) == [-1//12, 4//3, -5//2, 4//3, -1//12, 0//1]
+    @test Jecco.calculate_weights(2,6,2) == [-13//180, 19//15, -7//3, 10//9,
                                        1//12, -1//15, 1//90]
 
     # backward 2nd order derivatives i.e. ...,-1,0
-    @test calculate_weights(2,2,2) == [1//1, -2//1, 1//1]
-    @test calculate_weights(2,3,3) == [-1//1, 4//1, -5//1, 2//1]
-    @test calculate_weights(2,4,4) == [11//12, -14//3, 19//2, -26//3, 35//12]
+    @test Jecco.calculate_weights(2,2,2) == [1//1, -2//1, 1//1]
+    @test Jecco.calculate_weights(2,3,3) == [-1//1, 4//1, -5//1, 2//1]
+    @test Jecco.calculate_weights(2,4,4) == [11//12, -14//3, 19//2, -26//3, 35//12]
 
     # backward 2nd order derivatives i.e. ...,-2,-1,0,1
-    @test calculate_weights(2,3,2) == [0//1, 1//1, -2//1, 1//1]
-    @test calculate_weights(2,4,3) == [-1//12, 1//3, 1//2, -5//3, 11//12]
-    @test calculate_weights(2,5,4) == [1//12, -1//2, 7//6, -1//3, -5//4, 5//6]
+    @test Jecco.calculate_weights(2,3,2) == [0//1, 1//1, -2//1, 1//1]
+    @test Jecco.calculate_weights(2,4,3) == [-1//12, 1//3, 1//2, -5//3, 11//12]
+    @test Jecco.calculate_weights(2,5,4) == [1//12, -1//2, 7//6, -1//3, -5//4, 5//6]
 
     # backward 2nd order derivatives i.e. ...,-3,-2,-1,0,1,2
-    @test calculate_weights(2,5,3) == [0//1, -1//12, 4//3, -5//2, 4//3, -1//12]
-    @test calculate_weights(2,6,4) == [1//90, -1//15, 1//12, 10//9, -7//3, 19//15, -13//180]
+    @test Jecco.calculate_weights(2,5,3) == [0//1, -1//12, 4//3, -5//2, 4//3, -1//12]
+    @test Jecco.calculate_weights(2,6,4) == [1//90, -1//15, 1//12, 10//9, -7//3, 19//15, -13//180]
 
     # centered 3rd order derivatives i.e ...,-3,-2,-1,0,1,2,3
-    @test calculate_weights(3,6,3) == [1//8, -1//1, 13//8, 0//1, -13//8, 1//1, -1//8]
+    @test Jecco.calculate_weights(3,6,3) == [1//8, -1//1, 13//8, 0//1, -13//8, 1//1, -1//8]
 
     # forward 3rd order derivatives i.e 0,1,2,3,...
-    @test calculate_weights(3,3,0) == [-1//1, 3//1, -3//1, 1//1]
-    @test calculate_weights(3,4,0) == [-5//2, 9//1, -12//1, 7//1, -3//2]
+    @test Jecco.calculate_weights(3,3,0) == [-1//1, 3//1, -3//1, 1//1]
+    @test Jecco.calculate_weights(3,4,0) == [-5//2, 9//1, -12//1, 7//1, -3//2]
 
     # forward 3rd order derivatives i.e -1,0,1,2,...
-    @test calculate_weights(3,3,1) == [-1//1, 3//1, -3//1, 1//1]
-    @test calculate_weights(3,4,1) == [-3//2, 5//1, -6//1, 3//1, -1//2]
+    @test Jecco.calculate_weights(3,3,1) == [-1//1, 3//1, -3//1, 1//1]
+    @test Jecco.calculate_weights(3,4,1) == [-3//2, 5//1, -6//1, 3//1, -1//2]
 
     # backward 3rd order derivatives i.e -3,-2,-1,0,...
-    @test calculate_weights(3,3,3) == [-1//1, 3//1, -3//1, 1//1]
-    @test calculate_weights(3,4,4) == [3//2, -7//1, 12//1, -9//1, 5//2]
+    @test Jecco.calculate_weights(3,3,3) == [-1//1, 3//1, -3//1, 1//1]
+    @test Jecco.calculate_weights(3,4,4) == [3//2, -7//1, 12//1, -9//1, 5//2]
 
     # backward 3rd order derivatives i.e -2,-1,0,1...
-    @test calculate_weights(3,3,2) == [-1//1, 3//1, -3//1, 1//1]
-    @test calculate_weights(3,4,3) == [1//2, -3//1, 6//1, -5//1, 3//2]
+    @test Jecco.calculate_weights(3,3,2) == [-1//1, 3//1, -3//1, 1//1]
+    @test Jecco.calculate_weights(3,4,3) == [1//2, -3//1, 6//1, -5//1, 3//2]
 
 end
 
