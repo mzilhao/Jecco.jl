e09cdd9150748541e5f6ce1ffa7eeac997408b95
Miguel Zilhao <mzilhao@gmail.com>
2020-12-08 12:16:51 +0000

    KG_3_1 [types]: use FlattenedVector for Bulk and Boundary structs
    and remove old stuff

--- src/KG_3_1/types.jl
+++ src/KG_3_1/types.jl
@@ -10,7 +10,7 @@ Extend this type for different `InitialData` choices
 abstract type InitialData end
 
 
-struct BulkEvolved{T,A,S} <: AbstractPartition{3,A}
+struct BulkEvolved{T,A,S} <: FlattenedVector{T,3,A}
     x :: S
 end
 """
@@ -28,7 +28,7 @@ end
 getphi(ff::BulkEvolved) = ff.x.phi
 
 
-struct BulkConstrained{T,A,S} <: AbstractPartition{3,A}
+struct BulkConstrained{T,A,S} <: FlattenedVector{T,3,A}
     x :: S
 end
 """
@@ -52,7 +52,7 @@ getSd(ff::BulkConstrained)   = ff.x.Sd
 getA(ff::BulkConstrained)    = ff.x.A
 
 
-struct Boundary{T,A,S} <: AbstractPartition{3,A}
+struct Boundary{T,A,S} <: FlattenedVector{T,3,A}
     x :: S
 end
 """
@@ -118,101 +118,3 @@ function BulkConstrained(bulks::BulkPartition{N}) where{N}
     f = ntuple(i -> BulkConstrained(bulks[i]), N)
     BulkPartition(f)
 end
-
-
-
-# ### TODO: remove below
-
-
-# struct BulkVars{A}
-#     phi    :: A
-#     S      :: A
-#     Sd     :: A
-#     phid   :: A
-#     A      :: A
-#     dphidt :: A
-# end
-# BulkVars(phi, S, Sd, phid, A, dphidt) =  BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# function BulkVars(phi::Array{<:Number,N}) where {N}
-#     S      = similar(phi)
-#     Sd     = similar(phi)
-#     phid   = similar(phi)
-#     A      = similar(phi)
-#     dphidt = similar(phi)
-#     BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# end
-
-# BulkVars(phis::Vector) = [BulkVars(phi) for phi in phis]
-
-# function Base.getindex(bulk::BulkVars, i::Int)
-#     phi    = bulk.phi[i]
-#     S      = bulk.S[i]
-#     Sd     = bulk.Sd[i]
-#     phid   = bulk.phid[i]
-#     A      = bulk.A[i]
-#     dphidt = bulk.dphidt[i]
-#     BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# end
-
-# function Base.getindex(bulk::BulkVars, kr::AbstractRange)
-#     phi    = bulk.phi[kr]
-#     S      = bulk.S[kr]
-#     Sd     = bulk.Sd[kr]
-#     phid   = bulk.phid[kr]
-#     A      = bulk.A[kr]
-#     dphidt = bulk.dphidt[kr]
-#     BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# end
-
-# function Base.getindex(bulk::BulkVars, I::Vararg)
-#     phi    = bulk.phi[I...]
-#     S      = bulk.S[I...]
-#     Sd     = bulk.Sd[I...]
-#     phid   = bulk.phid[I...]
-#     A      = bulk.A[I...]
-#     dphidt = bulk.dphidt[I...]
-#     BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# end
-
-# function Base.getindex(bulk::BulkVars, ::Colon)
-#     phi    = bulk.phi[:]
-#     S      = bulk.S[:]
-#     Sd     = bulk.Sd[:]
-#     phid   = bulk.phid[:]
-#     A      = bulk.A[:]
-#     dphidt = bulk.dphidt[:]
-#     BulkVars{typeof(phi)}(phi, S, Sd, phid, A, dphidt)
-# end
-
-# Base.lastindex(bulk::BulkVars) = lastindex(bulk.phi)
-# Base.lastindex(bulk::BulkVars, i::Int) = lastindex(bulk.phi, i)
-
-# function setup(par_base)
-#     global VV = Potential(par_base)
-# end
-
-
-# struct BoundaryVars{A}
-#     a4   :: A
-# end
-
-# mutable struct AllVars{T}
-#     u        :: T
-
-#     phi_d0   :: T
-#     phi_du   :: T
-#     phi_dxx  :: T
-#     phi_dyy  :: T
-
-#     Sd_d0    :: T
-
-#     phid_d0  :: T
-#     phid_du  :: T
-
-#     A_d0     :: T
-# end
-# function AllVars{T}() where {T<:AbstractFloat}
-#     N = 1 + 4 + 1 + 2 + 1
-#     array = zeros(N)
-#     AllVars{T}(array...)
-# end
