38ecafc513e0066341261d72690759a23e357f56
Mikel-Sanchez-Garitaonandia <msgaritaonandia@gmail.com>
2021-07-16 19:00:30 +0200

    Printing the files instead of keeping the whole info in an array

--- examples/test_GW.jl
+++ examples/test_GW.jl
@@ -2,14 +2,17 @@ using Jecco
 using Jecco.AdS5_3_1
 using Plots
 gr()
-include("../src/AdS5_3_1/GW.jl")
-#/Users/apple/Dropbox/CollisionNewPotential/3+1 project/runs/spinodal2D_model1_en0.9_run01/#
-dirname = "/home/mikel/Documents/Jecco.jl/data/spinodal2D_e_1.0_L_20_N_80_AH_0.95"
-dt = 0.08
-alg = AB3()
-@time h, h_t, t_2, kxx, kyy, pxk, pyk, pzk = solve_GW(dirname, dt=dt, alg=alg, tol=10^-7)
+include("../src/AdS5_3_1/GW_2.jl")
+#dirname = "/home/mikel/Dropbox/CollisionNewPotential/GW/bubble_collisions/data/2D_bubble_collision_Lx_120_Ly_100_Nx_300_Ny_250/"
+dirname = "/home/mikel/Documents/PhD/gws_notes/2D_bubble_collision_Lx_120_Ly_100_Nx_300_Ny_250"
+outdir  = "/home/mikel/Documents/PhD/gws_notes/test/"
+dt = 0.1
+alg = AdS5_3_1.AB3()
+#@time h, h_t, t_2, kxx, kyy, pxk, pyk, pzk = solve_GW(dirname, dt=dt, alg=alg, tol=10^-10)
+@time solve_GW(outdir, dirname, dt=dt, alg=alg, tol=10^-20)
 nothing
-
+#=
 plot(t_2, real.(h[:,2,2,1]), lw=3)
 plot!(t_2, real.(pxk[:,2,2]), lw=3)
 xlabel!("t")
+=#
