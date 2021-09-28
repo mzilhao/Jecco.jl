7b5c1fafc8825ceaa24a3149168fba36a395bbb2
Mikel-Sanchez-Garitaonandia <msgaritaonandia@gmail.com>
2021-09-28 15:20:19 +0200

    Convert the gifs to mp4 using ffmpeg

--- scripts/plots.jl
+++ scripts/plots.jl
@@ -3,17 +3,19 @@ using Plots
 pyplot()
 
 dirname    = "/Users/apple/Documents/Jecco.jl/data/spinodal_2/"
-out_file = "/Users/apple/Dropbox/CollisionNewPotential/GW/spinodal/fourier_modes/"
+out_file   = "/Users/apple/Dropbox/CollisionNewPotential/GW/spinodal/GW_movies/"
+video_name = "spinodal_2_e_T2_h2"
 
-#=
+
 en         = VEVTimeSeries(dirname, :energy)
 T2         = AdS5_3_1.TTTimeSeries(dirname, :T2)
-#hd2        = GWTimeSeries(dirname, :hd2)
+hd2        = GWTimeSeries(dirname, :hd2)
 t, x, y    = get_coords(en, :, :, :)
 Nt, Nx, Ny = size(en)
-Ntt, _, _ = size(T2)
+Nt2, _, _  = size(T2)
+Nt3, _, _  = size(hd2)
 
-nt = minimum((Ntt, Nt))
+nt = minimum((Nt, Nt2, Nt3))
 
 
 
@@ -30,6 +32,9 @@ end
 
 xcord = reshape(xx, Nx*Ny)
 ycord = reshape(yy, Nx*Ny)
+
+
+
 emax   = maximum(en[:,:,:])
 emin   = minimum(en[:,:,:])
 hd2min = minimum(hd2[:,:,:])
@@ -39,9 +44,9 @@ hd2max = maximum(hd2[:,:,:])
 
 
 
-anim = @animate for n in 1:Nt
+anim = @animate for n in 1:nt
     @time begin
-        println("progress = $(Int(floor(n/Nt*100)))%")
+        println("progress = $(Int(floor(n/nt*100)))%")
         een = reshape(en[n,:,:], Nx*Ny)
         p1  = surface(xcord[1:20:end], ycord[1:20:end], een[1:20:end], xlabel="xΛ", ylabel="yΛ", zlabel="ℰ/Λ⁴",color=:jet, zlim=(emin,emax), clims=(emin,emax), camera=(50,50), title="tΛ = $(t[n])")
         nothing
@@ -61,58 +66,65 @@ end
 
 
 
-#If x and y are of same length
-emax   = maximum(en[:,:,:])
-emin   = minimum(en[:,:,:])
-#T2min  = minimum(T2[:,:,:])
-#T2max  = maximum(T2[:,:,:])
+#SQUARE BOXES
+emax   = 1.8
+emin   = 0.2
+hd2min = 0.0
+hd2max = 0.5
 
 anim = @animate for n in 1:nt
     @time begin
         println("progress = $(n/nt*100)%")
         p1  = surface(x, y, en[n,:,:], xlabel="xΛ", ylabel="yΛ", zlabel="ℰ/Λ⁴",color=:jet, zlim=(emin,emax), clims=(emin,emax), camera=(50,50), title="tΛ = $(t[n])")
-        #plot(p1, size=(2000,2000))
         nothing
         T2min = minimum(T2[n,:,:])
         T2max = maximum(T2[n,:,:])
         p2 = surface(x, y, T2[n,:,:], xlabel="xΛ", ylabel="yΛ", zlabel="T²/Λ⁸",color=:jet, zlim=(T2min,T2max), clims=(T2min,T2max), camera=(50,50), title="tΛ = $(t[n])")
         nothing
-        plot(p1, p2, size=(2000,1000))
+        p3 = surface(x, y, hd2[n,:,:], xlabel="xΛ", ylabel="yΛ", zlabel="hd²/Λ⁸",color=:jet, zlim=(hd2min,hd2max), clims=(hd2min,hd2max), camera=(50,50), title="tΛ = $(t[n])")
+        nothing
+        plot(p1, p2, p3, size=(3000,2000))
         nothing
     end
 end
 
-gif(anim, out_file*"spinodal_a4_e_T2.gif", fps=12)
+gif(anim, out_file*video_name*".gif", fps=12)
+run(`rm $(out_file*video_name*".mp4")`)
+run(`ffmpeg -i $(out_file*video_name*".gif") -pix_fmt yuv420p $(out_file*video_name*".mp4")`)
+run(`rm $(out_file*video_name*".gif")`)
 
-=#
 
+
+
+#=
 #Fourier mode plots
-nxmax, nymax = (4, 4)
+nxmax, nymax = (3, 3)
+#=
 @time a, b, c, d  = AdS5_3_1.Fourier_cos_sin(dirname, :energy)
 println("Max cos_cos: $(findmax(abs.(a))[2])")
 println("Max cos_sin: $(findmax(abs.(b))[2])")
 println("Max sin_cos: $(findmax(abs.(c))[2])")
 println("Max sin_sin: $(findmax(abs.(d))[2])")
-
 _, Nkx, Nky = size(a)
 a4          = BoundaryTimeSeries(dirname, :a4)
 t, _, _     = get_coords(a4, :, 1, 1)
-#idx         = findfirst(t .> 100)
-#println("Max cos_cos: $(findmax(abs.(a[1:idx,:,:]))[2])")
-#println("Max cos_sin: $(findmax(abs.(b[1:idx,:,:]))[2])")
-#println("Max sin_cos: $(findmax(abs.(c[1:idx,:,:]))[2])")
-#println("Max sin_sin: $(findmax(abs.(d[1:idx,:,:]))[2])")
-idx = length(t)
+=#
+idx         = findfirst(t .> 100)
+println("Max cos_cos: $(findmax(abs.(a[1:idx,:,:]))[2])")
+println("Max cos_sin: $(findmax(abs.(b[1:idx,:,:]))[2])")
+println("Max sin_cos: $(findmax(abs.(c[1:idx,:,:]))[2])")
+println("Max sin_sin: $(findmax(abs.(d[1:idx,:,:]))[2])")
 p1 = plot()
 p2 = plot()
 p3 = plot()
 p4 = plot()
+legend_pos = :bottomright
 for j in 1:nxmax
     for i in 1:nymax
-        plot!(p1, t[1:idx], abs.(a[1:idx,i,j]), lw = 1, label="nx=$i ny=$j")
-        plot!(p2, t[1:idx], abs.(b[1:idx,i,j]), lw = 1, label="nx=$i ny=$j")
-        plot!(p3, t[1:idx], abs.(c[1:idx,i,j]), lw = 1, label="nx=$i ny=$j")
-        plot!(p4, t[1:idx], abs.(d[1:idx,i,j]), lw = 1, label="nx=$i ny=$j")
+        plot!(p1, t[1:idx], log.(abs.(a[1:idx,i,j])), lw = 1, label="nx=$i ny=$j", legend = legend_pos)
+        plot!(p2, t[1:idx], log.(abs.(b[1:idx,i,j])), lw = 1, label="nx=$i ny=$j", legend = legend_pos)
+        plot!(p3, t[1:idx], log.(abs.(c[1:idx,i,j])), lw = 1, label="nx=$i ny=$j", legend = legend_pos)
+        plot!(p4, t[1:idx], log.(abs.(d[1:idx,i,j])), lw = 1, label="nx=$i ny=$j", legend = legend_pos)
     end
 end
 xlabel!(p1, "tΛ")
@@ -126,10 +138,11 @@ title!(p4, "Sin*Sin")
 
 pfinal = plot(p1, p2, p3, p4, size=(1000, 1000))
 nothing
-savefig(pfinal, out_file*"spinodal_a4.pdf")
+savefig(pfinal, out_file)
 #=
 savefig(p1, out_file*"spinodal_2_cos_cos.pdf")
 savefig(p2, out_file*"spinodal_2_cos_sin.pdf")
 savefig(p3, out_file*"spinodal_2_sin_cos.pdf")
 savefig(p4, out_file*"spinodal_2_sin_sin.pdf")
+=#
 =#
